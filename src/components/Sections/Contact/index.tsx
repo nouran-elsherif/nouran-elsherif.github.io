9dc4f6a05e4eaa5f9da76bfabb1acb43da4ba913
Nouran Elsherif <nouran.a.elsherif@gmail.com>
2023-12-26 22:18:25 +0200

    contact index

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -47,7 +47,7 @@ const Contact: FC = memo(() => {
                     <dd className="flex items-center">
                       <a
                         className={classNames(
-                          '-m-2 flex rounded-md p-2 text-neutral-300 hover:text-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500',
+                          '-m-2 flex rounded-md p-2 text-neutral-300 hover:text-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-700',
                           {'hover:text-white': href},
                         )}
                         href={href}
