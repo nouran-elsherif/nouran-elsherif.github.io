76f3b2e3e17e6fcce526fd032a9b496e2aed70c1
Nouran Elsherif <nouran.a.elsherif@gmail.com>
2023-12-26 22:20:45 +0200

    data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -15,10 +15,10 @@ import LinkedInIcon from '../components/Icon/LinkedInIcon';
 // import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import nudgelabs from '../images/portfolio/nudgelabs.png';
-import racemate from '../images/portfolio/racemate.webp';
-import gurow from '../images/portfolio/gurow.webp';
+import racemate from '../images/portfolio/racemate.png';
+import gurow from '../images/portfolio/gurow.png';
 // import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import profilepic from '../images/profilepic.jpg';
+import portrait from '../images/Portrait.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -66,14 +66,14 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I am an Egyptian <strong className="text-stone-100">Senior Mobile Application Engineer</strong>, currently working
-        at <strong className="text-stone-100">Optomatica</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I am a <strong className="text-stone-100">Senior Mobile Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Optomatica</strong> developing robust cross-platform mobile applications using Flutter and React Native.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
+        In my free time, you can see me trying out new activities and places.
+        {/* In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
         plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        <strong className="text-stone-100">Vancouver Island</strong>. */}
       </p>
     </>
   ),
@@ -96,7 +96,7 @@ export const heroData: Hero = {
  * About section
  */
 export const aboutData: About = {
-  profileImageSrc: profilepic,
+  profileImageSrc: portrait,
   description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
@@ -105,7 +105,7 @@ export const aboutData: About = {
     {label: 'Age', text: '28', Icon: CalendarIcon},
     {label: 'Nationality', text: 'Egyptian', Icon: FlagIcon},
     {label: 'Interests', text: 'Art, Swimming', Icon: SparklesIcon},
-    {label: 'Study', text: 'Ain Shams University', Icon: AcademicCapIcon},
+    {label: 'Education', text: 'Ain Shams University', Icon: AcademicCapIcon},
     {label: 'Employment', text: 'Optomatica', Icon: BuildingOffice2Icon},
   ],
 };
@@ -224,20 +224,20 @@ export const skills: SkillGroup[] = [
 export const portfolioItems: PortfolioItem[] = [
   {
     title: 'NudgeLabs',
-    description: 'A wellbeing-app',
+    description: 'A wellbeing-app.\n I was responsible for managing and developing the initial version of NudgeLabs application.',
     url: 'https://nudgelabs.com/',
     image: nudgelabs,
   },
   {
     title: 'Racemate',
-    description: 'Digital running companion',
+    description: 'Digital running companion.\n I was responsible for developing multiple features in Racemate app, including but not ',
     url: 'https://racemate.ai',
     image: racemate,
   },
   {
     title: 'GuRow AI',
-    description: 'Smart financial Advisor',
-    url: 'https://reactresume.com',
+    description: 'Smart financial Advisor.\n I developed the GuRow AI which helped users figure out which assets to invest in based on their risk tolerance.',
+    url: 'https://arabfinance.com/News/newdetails/4844',
     image: gurow,
   },
   {
@@ -299,14 +299,14 @@ export const education: TimelineItem[] = [
     location: 'Faculty of Computer And Information Sciences, Ain Shams University',
     title: 'Pre-Masters in Software Engineering',
     url:"https://cis.asu.edu.eg/",
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    content: <p>I acquired vital skills essential for excelling in advanced academic studies. These included sharpening my research abilities, improving my academic writing, and developing a strong foundation in critical thinking.</p>,
   },
   {
     date: 'Sep 2013-May 2017',
     location: 'Faculty of Computer And Information Sciences, Ain Shams University',
     title: 'Bachelors of Computer Science',
     url:"https://cis.asu.edu.eg/",
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    content: <p>Completing my bachelor's degree in Software Engineering provided me with a comprehensive understanding of programming languages, software development methodologies, and systems analysis. I honed my problem-solving skills by working on diverse projects, tackling real-world challenges in areas such as database management and software testing. The program not only equipped me with technical expertise but also fostered effective communication and collaboration within a team setting. </p>,
   },
 ];
 
@@ -318,8 +318,9 @@ export const experience: TimelineItem[] = [
     url:'https://optomatica.com/',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Worked and managed multiple projects in Flutter and React Native. Designed robust project architectures, ensuring scalability and maintainability tailored to specific project requirements.<br/>
+        <strong>Stack:</strong> Flutter, Dart, React Native, JavaScript, TypeScript, NestJS, State management(Riverpod, MobX, Provider), Databases (SQLite, Prisma, GraphQL), Firebase tools (Firestore, Firebase Auth, Google CrashLytics and Analytics).<br/>
+        <strong>Projects:</strong> NudgeLabs, Racemate, Gurow AI, Gate ID. 
       </p>
     ),
   },
@@ -330,8 +331,9 @@ export const experience: TimelineItem[] = [
     url:"https://optomatica.com/",
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Learned React Native and Flutter. Pioneered implementation of Widget testing to improve testing efficiency and time.<br/>
+        <strong>Stack:</strong> React Native, JavaScript, TypeScript, State management(MobX, Provider), Databases (SQLite), OAuth, Firebase tools (Firestore, Firebase Auth, Google CrashLytics and Analytics).<br/>
+        <strong>Projects:</strong> RaceFox, Kaching, NudgeLabs. 
       </p>
     ),
   },
@@ -339,26 +341,50 @@ export const experience: TimelineItem[] = [
     date: 'Sep 2017-Sep 2020',
     location: 'Faculty of Computer and Information Sciences, Ain Shams University',
     title: 'Teaching Assistant',
-    url:"https://optomatica.com/",
+    url:"https://cis.asu.edu.eg/",
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      Designed and delivered engaging courses, fostering interactive learning that addressed diverse student needs at various proficiency levels. Guided and mentored students to enhance academic performance and equip them with essential skills for future success.<br/>
+      <strong>Skills gained:</strong> Communication, mentorship, leadership, evaluation, presentation, and public speaking.<br/>
+    </p>
     ),
   },
   {
     date: 'Mar 2018-Dec 2018',
     location: 'Digizilla',
-    title: 'Junior Software Engineer',
-    url:"https://optomatica.com/",
+    title: 'Software Engineer',
+    url:"https://Digizilla.net/",
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+      Implemented and customized Odoo modules using Python. Learned React Native. Helped and mentored interns and newly employed developers learn Odoo module development.<br/>
+      <strong>Stack:</strong> Odoo, Python, React Native, JavaScript.<br/>
+      {/* <strong>Projects:</strong> RaceFox, Kaching, NudgeLabs.  */}
+    </p>
+    ),
+  },
+  {
+    date: 'Aug 2016',
+    location: 'Ana Trainee Program by Vodafone',
+    title: 'Trainee',
+    url:"https://vodafone.com.eg/",
+    content: (
+      <p>
+      Was assigned to Sharepoint team, learned a lot about what they do and how they manage internal sharepoints sites and forms.
       </p>
     ),
   },
+  {
+    date: 'Aug 2015',
+    location: 'Ana Trainee Program by Vodafone',
+    title: 'Trainee',
+    url:"https://vodafone.com.eg/",
+    content: (
+      <p>
+      Was assigned to Business Intelligence team, learned a lot about what they do and how they manage customer promotions.
+      </p>
+    ),
+  },
+  
 ];
 
 /**
